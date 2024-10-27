710384f9a152bc268df75247e21fd0cd72e46b73
Mary-Paz-AN <marypaz.an.0@gmail.com>
2024-10-26 23:14:37 -0600

    agregar hechos version final

--- programa/BC.pl
+++ programa/BC.pl
@@ -1,18 +1,24 @@
-% se agregan destinos, actividades y asociar_actividad para que la base de datos tenga algunoos al iniciar el programa %
-
-% destino/2 %
 :- dynamic destino/2.
+:- discontiguous destino/2.
+:- dynamic actividad/5.
+:- discontiguous actividad/5.
+:- dynamic asociar_actividad/2.
+:- discontiguous asociar_actividad/2.
+
 destino(paris, 'Ciudad de la Luz').
 destino(nueva_york, 'La Gran Manzana').
-
-% actividad/5 %
-:- dynamic actividad/5.
 actividad(museo_louvre, 25, 2, 'Visitar el Museo del Louvre', ['arte', 'historia']).
 actividad(paseo_en_bici, 30, 3, 'Paseo en bicicleta por la ciudad', ['aventura', 'naturaleza']).
 actividad(paseo_en_carro, 30, 3, 'Paseo en carro', ['aventura', 'naturaleza']).
-
-% asociar_actividad/2 %
-:- dynamic asociar_actividad/2.
 asociar_actividad(paris, museo_louvre).
 asociar_actividad(paris, paseo_en_carro).
 asociar_actividad(nueva_york, paseo_en_bici).
+destino(tokyo, 'Cuidad de la Tecnologia').
+destino(seul, 'Cuidad del Skincare').
+destino(limon, 'Cuidad de las mejores playas').
+actividad(playa, 100, 4, 'Perfecta para relajarse', ['aventura', 'diversion']).
+actividad(concierto, 2000, 2, 'Cantar hasta no poder', ['experiencia', 'diversion']).
+asociar_actividad(tokyo, concierto).
+asociar_actividad(limon, playa).
+actividad(karaoke, 500, 3, 'Para pasar el rato', ['experiencia', 'diversion']).
+actividad(ver_pajaros, 120, 2, 'Pasar una tarde relajante', ['educacion', 'naturaleza', 'panorama', 'cultura']).
--- programa/programa.pl
+++ programa/programa.pl
@@ -3,119 +3,231 @@
 
 % Guarda los hechos de destino al archivo %
 guardarDestino(Nombre, Descripcion) :- 
+    assertz(destino(Nombre, Descripcion)),
+
     % Abrir el archivo en modo de agregar sin borrar el contenido
     open('BC.pl', append, Stream),
     
     % Escribir el hecho en el archivo
     write(Stream, 'destino('),
-    write(Stream, Ciudad),
+    write(Stream, Nombre),
     write(Stream, ', \''),
     write(Stream, Descripcion),
-    write(Stream, '\').'),
+    write(Stream, '\''),  
+    write(Stream, ').'), 
     nl(Stream), 
     close(Stream). % Cerrar el archivo
 
+% Escribir en el archivo el las categorias en la lista
+guardarTipos(Stream, [Ultimo]) :-
+    write(Stream, '\''),
+    write(Stream, Ultimo),
+    write(Stream, '\'').
+
+guardarTipos(Stream, [Cabeza | Resto]) :-
+    write(Stream, '\''),
+    write(Stream, Cabeza),
+    write(Stream, '\''),
+    write(Stream, ', '),
+    guardarTipos(Stream, Resto).
+
 % Guarda los hechos de actividad al archivo %
 guardarActividad(Nombre, Costo, Duracion, Descripcion, Tipos) :- 
+    assertz(actividad(Nombre, Costo, Duracion, Descripcion, Tipos)),
+
     % Abrir el archivo en modo de agregar sin borrar el contenido
     open('BC.pl', append, Stream),
     
     % Escribir el hecho en el archivo
     write(Stream, 'actividad('),
     write(Stream, Nombre),
-    write(Stream, ', \''),
+    write(Stream, ', '),
     write(Stream, Costo),
-    write(Stream, ', \''),
+    write(Stream, ', '),
     write(Stream, Duracion),
     write(Stream, ', \''),
     write(Stream, Descripcion),
-    write(Stream, ', \''),
-    write(Stream, Tipos),
-    write(Stream, '\').'),
-    nl(Stream), 
-    close(Stream). % Cerrar el archivo
+    write(Stream, '\''),
+    write(Stream, ', ['),
+    guardarTipos(Stream, Tipos),
+    write(Stream, ']).'), 
+    nl(Stream),    
+    close(Stream).
 
 % Guarda los hechos de asociar_actividad al archivo %
 guardarAsociarActividad(Destino, Actividad) :- 
+    assertz(asociar_actividad(Destino, Actividad)),
+
     % Abrir el archivo en modo de agregar sin borrar el contenido
     open('BC.pl', append, Stream),
     
     % Escribir el hecho en el archivo
     write(Stream, 'asociar_actividad('),
     write(Stream, Destino),
-    write(Stream, ', \''),
+    write(Stream, ', '),
     write(Stream, Actividad),
-    write(Stream, '\').'),
+    write(Stream, ').'), 
     nl(Stream), 
     close(Stream). % Cerrar el archivo
 
+% Verifica que el destino exista en la base de conocimiento
+verificarDestino(Destino) :-
+    destino(Destino, _).
+
+% Verifica que la actividad exista en la base de conocimiento
+verificarActividad(Actividad) :-
+    actividad(Actividad, _, _, _, _).
+
 % Pregunta por la información del destino %
 agregarDestino :- 
-    write('Nombre del destino: '),
+    write('Ingrese el nombre del destino: '),
     read(Nombre), nl,
+    (   verificarDestino(Nombre) -> 
+            write('El destino ya existe. Porfavor agregue otro.'), nl,
+            agregarDestino
+    ;   write('Escriba una breve descripción del destino: '), 
+            read(DescripcionAtom), nl,
 
-    % Pregunta por la descripción y la convierto en una cadena de texto %
-    write('Escriba una brve descripion del destino: '), 
-    read_line_to_string(user_input, Descripcion), nl,
-
-    % Guarda el destino en el archivo y en la base de conocimiento %
-    assertz(destino(Nombre, Descripcion)),
-    guardarDestino(Nombre, Descripcion),
-    write('Destino registrado exitosamente.'), nl, nl.
-
-% Pregunta por la informacion de actividad %
-agregarActividad :-
-    % Pregunta por el nombre de la actividad %
-    write('Nombre de la actividad: '),
-    read(Nombre), nl,
-
-    % Pregunta por el costo y verifica que sea un entero %
-    write('Ingrese el costo de la actividad por persona: '),
-    read(Costo), nl,
-    (integer(Costo) -> 
-        % Si el costo es un entero, preguntar por los días
-        write('Ingrese la duración en días de la actividad: '),
-        read(Dias), nl,
-        (integer(Dias) ->
-            % Pregunta por la descripción y la convierte en una cadena de texto %
-            write('Escriba una breve descripción de la actividad: '), 
-            read_line_to_string(user_input, Descripcion), nl,
-
-            % Guarda la actividad en la base de conocimiento
-            write('Actividad registrada exitosamente.'), nl, nl
-        ;
-            % Si los días no son un entero, mostrar mensaje de error
-            write('Por favor ingrese un número entero válido para los días.'), nl, nl,
-            agregarActividad  % Volver a preguntar
-        )
-    ;
-        % Si el costo no es un entero, mostrar mensaje de error
-        write('Por favor ingrese un número entero válido para el costo.'), nl, nl,
-        agregarActividad  % Volver a preguntar
+            % Verifica que lo que se escribio es un string
+            atom_string(DescripcionAtom, Descripcion),
+            guardarDestino(Nombre, Descripcion),
+            write('Destino registrado exitosamente.'), nl, nl
     ).
 
-% Preguntapor la información para asociar la actividad (Falta verificar que el destino exista y tambien la actividad)%
+
+% Pregunta por el costo y verifica si es un entero
+preguntarXCostoActividad(Costo) :-
+    write('Ingrese el costo de la actividad: '),
+    read(CostoInput), nl,
+    
+    % Verificar si es entero
+    (   integer(CostoInput) -> Costo is CostoInput
+    ;   write('El costo debe ser un entero.'), nl,
+        preguntarXCostoActividad(Costo)
+    ).
+
+% Pregunta por la verificar y verifica si es un entero
+preguntarXDuracionActividad(Duracion) :-
+    write('Ingrese el duración de la actividad (dias): '),
+    read(DuracionInput), nl,
+    
+    % Verificar si es entero
+    (   integer(DuracionInput) -> Duracion is DuracionInput
+    ;   write('La duración debe ser un entero.'), nl,
+        preguntarXDuracionActividad(Duracion)
+    ).
+
+% Agrega categoria a la lista
+agregarCategoria(Elemento, Lista, [Elemento | Lista]).
+
+% Pregunta por la categoria que quiere agregar
+preguntarXTipoActividad(ListaTipo, ListaTipoFinal) :-
+    write('1. Arte'), nl,
+    write('2. Cultura'), nl,
+    write('3. Historia'), nl,
+    write('4. Arquitectura'), nl,
+    write('5. Panorama'), nl,
+    write('6. Diversión'), nl,
+    write('7. Romántico'), nl,
+    write('8. Gastronomía'), nl,
+    write('9. Naturaleza'), nl,
+    write('10. Educativo'), nl,
+    write('11. Experiencia'), nl,
+    write('12. Aventura'), nl,
+    write('0. Dejar de añadir'), nl,
+    write('Ingrese la categoria de la actividad: '),
+    read(Cat),
+    (   Cat = 1 -> 
+            agregarCategoria('arte', ListaTipo, NuevaListaTipo),
+            preguntarXTipoActividad(NuevaListaTipo, ListaTipoFinal)
+    ;   Cat = 2 -> 
+            agregarCategoria('cultura', ListaTipo, NuevaListaTipo),
+            preguntarXTipoActividad(NuevaListaTipo, ListaTipoFinal)
+    ;   Cat = 3 -> 
+            agregarCategoria('historia', ListaTipo, NuevaListaTipo),
+            preguntarXTipoActividad(NuevaListaTipo, ListaTipoFinal)
+    ;   Cat = 4 -> 
+            agregarCategoria('arquitectura', ListaTipo, NuevaListaTipo),
+            preguntarXTipoActividad(NuevaListaTipo, ListaTipoFinal)
+    ;   Cat = 5 -> 
+            agregarCategoria('panorama', ListaTipo, NuevaListaTipo),
+            preguntarXTipoActividad(NuevaListaTipo, ListaTipoFinal)
+    ;   Cat = 6 -> 
+            agregarCategoria('diversion', ListaTipo, NuevaListaTipo),
+            preguntarXTipoActividad(NuevaListaTipo, ListaTipoFinal)
+    ;   Cat = 7 -> 
+            agregarCategoria('romantico', ListaTipo, NuevaListaTipo),
+            preguntarXTipoActividad(NuevaListaTipo, ListaTipoFinal)
+    ;   Cat = 8 -> 
+            agregarCategoria('gastronomia', ListaTipo, NuevaListaTipo),
+            preguntarXTipoActividad(NuevaListaTipo, ListaTipoFinal)
+    ;   Cat = 9 -> 
+            agregarCategoria('naturaleza', ListaTipo, NuevaListaTipo),
+            preguntarXTipoActividad(NuevaListaTipo, ListaTipoFinal)
+    ;   Cat = 10 -> 
+            agregarCategoria('educacion', ListaTipo, NuevaListaTipo),
+            preguntarXTipoActividad(NuevaListaTipo, ListaTipoFinal)
+    ;   Cat = 11 -> 
+            agregarCategoria('experiencia', ListaTipo, NuevaListaTipo),
+            preguntarXTipoActividad(NuevaListaTipo, ListaTipoFinal)
+    ;   Cat = 12 -> 
+            agregarCategoria('aventura', ListaTipo, NuevaListaTipo),
+            preguntarXTipoActividad(NuevaListaTipo, ListaTipoFinal)
+    ;   Cat = 0 ->
+            (   ListaTipo \= [] -> 
+                ListaTipoFinal = ListaTipo,
+                write('Categorías añadidas exitosamente.'), nl
+            ;   write('Agregue al menos una categoría antes de salir.'), nl,
+                preguntarXTipoActividad(ListaTipo, ListaTipoFinal)
+            )
+    ;   write('Opción inválida. Vuelva a intentarlo.'), nl,
+        preguntarXTipoActividad(ListaTipo, ListaTipoFinal)
+    ).
+
+    
+% Pregunta por la informacion de actividad %
+agregarActividad :-
+    write('Ingrese el nombre de la actividad: '),
+    read(Nombre), nl,
+    (   verificarActividad(Nombre) ->
+            write('La actividad ya existe. Porfavor, agregue otra.'), nl,
+            agregarActividad
+    ;   preguntarXCostoActividad(Costo),
+            preguntarXDuracionActividad(Duracion),
+
+            write('Escriba una breve descripcion de la actividad: '),
+            read(DescripAtm), nl,
+            atom_string(DescripAtm, Descripcion),
+
+            preguntarXTipoActividad([], ListaTipoFinal),
+
+            guardarActividad(Nombre, Costo, Duracion, Descripcion, ListaTipoFinal),
+            write('Destino registrado exitosamente.'), nl, nl
+    ).
+
+% Pregunta por la actividad y verificar que exista
+preguntarActividadAA(Actividad) :-
+    write('Ingrese el nombre de la actividad: '),
+    read(ActividadInput), nl,
+
+    (   verificarActividad(ActividadInput) ->
+            Actividad = ActividadInput
+    ;   write('La actividad no existe. Porfavor ingrese una existente.'), nl,
+            preguntarActividadAA(Actividad)
+    ).
+
+% Preguntapor la información para asociar la actividad 
 agregarAsociarActividad :- 
-    write('Nombre del destino: '),
+    write('Ingrese el nombre del destino: '),
     read(Destino), nl,
 
-    % Verifica si el destino existe
-    (destino(Destino, _) -> 
-        write('Nombre de la actividad: '),
-        read(Actividad), nl,
-
-        % Verifica si la actividad existe
-        (actividad(Actividad, _, _, _, _) ->  
-            assertz(asociar_actividad(Destino, Actividad)),
+    % Verificar que el destino exista
+    (   verificarDestino(Destino) ->
+            preguntarActividadAA(Actividad),
             guardarAsociarActividad(Destino, Actividad),
-            write('Actividad asociada correctamente.'), nl, nl
-        ;
-            write('La actividad no existe.'), nl, nl,
-            agregarAsociarActividad  
-        )
-    ;
-        write('El destino no existe.'), nl, nl,
-        agregarAsociarActividad 
+            write('Actividad asociada exitosamente.'), nl, nl
+    ;   write('El destino no existe. Porfavor ingrese uno existente.'), nl,
+            agregarAsociarActividad
     ).
 
 % Menú para agregar hechos %
@@ -134,7 +246,6 @@ manejarOpcionHechos(b) :- agregarActividad, menuAgregarHechos.
 manejarOpcionHechos(c) :- agregarAsociarActividad, menuAgregarHechos.
 manejarOpcionHechos(d) :- !, true.
 manejarOpcionHechos(_) :- write('Opción inválida, vuelva a intentarlo.'), nl, nl, menuAgregarHechos.
-
 
 % Consultar destino %
 consultarDestino :- write("Consultar destino"), nl.
